2f92c2393d5093c5e0759bdc3d7db5ce7d68a192
27medMo7sen <am6945@fayoum.edu.eg>
2024-10-16 09:21:17 +0300

    Ignore new environment variable configuration files and update .gitignore

--- Back-end/index.js
+++ Back-end/index.js
@@ -3,7 +3,7 @@ import path from "path";
 import { config } from "dotenv";
 const app = express();
 const PORT = 3000;
-config({ path: path.resolve("./config/config.env", ".env") });
+config({ path: path.resolve("./Config/config.env") });
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World");
