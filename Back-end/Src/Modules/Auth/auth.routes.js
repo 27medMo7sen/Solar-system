6058c3f3ee3dba725eba7aae8b0679eebe9ce01c
27medMo7sen <am6945@fayoum.edu.eg>
2024-10-17 13:14:52 +0300

    Add Auth controller and routes

---
+++ Back-end/Src/Modules/Auth/auth.routes.js
@@ -0,0 +1,5 @@
+import * as ac from "./auth.controller.js";
+import { Router } from "express";
+import { asyncHandler } from "../../Utils/errorhandling.js";
+const router = Router();
+export default router;
