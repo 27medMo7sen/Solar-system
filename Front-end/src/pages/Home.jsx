244aa78758aaa7fd05339b151cf69240f5e08a5f
27medMo7sen <am6945@fayoum.edu.eg>
2024-10-20 14:49:06 +0300

    Add Home page with Hero component

---
+++ Front-end/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+import Hero from "../components/Slider/Slider";
+
+export const Home = () => {
+  return (
+    <Fragment>
+      <Hero />
+    </Fragment>
+  );
+};
