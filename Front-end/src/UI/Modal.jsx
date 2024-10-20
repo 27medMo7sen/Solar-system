4f07428162472e303da8506f79986c1a7f6318e2
27medMo7sen <am6945@fayoum.edu.eg>
2024-10-20 14:48:46 +0300

    Refactor UI components and add Modal and SideNavbar components

---
+++ Front-end/src/UI/Modal.jsx
@@ -0,0 +1,55 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return (
+    <div
+      className="fixed top-0 left-0 w-full h-full bg-black/50 z-20"
+      onClick={props.onClose}
+    />
+  );
+};
+const ModalOverlay = (props) => {
+  return (
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 animate-slideDown bg-white z-30 p-3 rounded-lg">
+      {props.children}
+    </div>
+  );
+};
+const SideModalOverlay = (props) => {
+  return (
+    <div className="fixed top-0 h-screen w-60 right-0 animate-slideLeft bg-white z-30  rounded-lg">
+      {props.children}
+    </div>
+  );
+};
+const portalElement = document.getElementById("overlays");
+
+export const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+export const SideModal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <SideModalOverlay>{props.children}</SideModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
---
+++ Front-end/src/components/SideNavebar/SideNavbar.jsx
@@ -0,0 +1,58 @@
+import { NavLink } from "react-router-dom";
+export const SideNavbar = () => {
+  const activeClass = "h-10 w-full flex items-center border-b-2 text-green-300";
+  const inactiveClass =
+    "h-10 w-full flex items-center border-b-2 hover:text-green-300 transition ease-in-out delay-150 over:-translate-y-1 hover:scale-110";
+  return (
+    <div className="text-black pl-5  flex flex-col  justify-center   items-center ">
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? activeClass : inactiveClass)}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/products"
+        className={({ isActive }) => (isActive ? activeClass : inactiveClass)}
+      >
+        Products
+      </NavLink>
+      <NavLink
+        to="/services"
+        className={({ isActive }) => (isActive ? activeClass : inactiveClass)}
+      >
+        Services
+      </NavLink>
+      <NavLink
+        to="/become-a-partner"
+        className={({ isActive }) => (isActive ? activeClass : inactiveClass)}
+      >
+        Become a Partner
+      </NavLink>
+      <NavLink
+        to="/solar-calculator"
+        className={({ isActive }) => (isActive ? activeClass : inactiveClass)}
+      >
+        Solar Calculator
+      </NavLink>
+      <NavLink
+        to="/blog"
+        className={({ isActive }) => (isActive ? activeClass : inactiveClass)}
+      >
+        Blog
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({ isActive }) => (isActive ? activeClass : inactiveClass)}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/contact"
+        className={({ isActive }) => (isActive ? activeClass : inactiveClass)}
+      >
+        Contact Us
+      </NavLink>
+    </div>
+  );
+};
