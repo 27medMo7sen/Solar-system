712f3494119a80b28d8008d820cead0117c094f1
foash-111 <mr2221@fayoum.edu.eg>
2024-10-29 22:58:09 +0300

    categories list dommy component

--- Front-end/src/components/Categories/CategoriesList.jsx
+++ Front-end/src/components/Categories/CategoriesList.jsx
@@ -1,6 +1,8 @@
 import { categories } from "../../Data";
 import { ProductCard } from "../General/ProductCard";
 import { ProductList } from "../General/ProductList";
+
+
 export const CategoriesList = () => {
   return <div className="flex flex-col gap-2 ml-28 mx-lg:ml-10 mx-md:ml-0">
     {categories.map((category, index) => (
