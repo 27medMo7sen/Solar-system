b226662475720df230d4482eae5b0978b7efd82f
Ahmed mohsen <am6945@fayoum.edu.eg>
2024-10-29 23:32:13 +0300

    Refactor routing and remove unused components

--- Front-end/src/pages/PartnershipPage.jsx
+++ Front-end/src/pages/PartnershipPage.jsx
@@ -1,13 +1,8 @@
 import { Fragment } from "react";
-import { Pathbar } from "../components/General/Pathbar";
 
 export const PartnershipPage = () => {
-  const path = [
-    { name: "Partnership", link: "/partnership" },
-  ];
   return (
     <Fragment>
-      <Pathbar path={path} />
     </Fragment>
   );
 };
