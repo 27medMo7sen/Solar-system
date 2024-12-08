a17e1167e33f7c9c81ae40994c96b6bb802688db
Ahmed Mohsen <am6945@fayoum.edu.eg>
2024-12-08 15:25:30 +0200

    Update CORS middleware in initiateApp.js to allow requests from https://solarease.vercel.app and comment out localhost configuration

--- Back-end/Src/Utils/initiateApp.js
+++ Back-end/Src/Utils/initiateApp.js
@@ -7,14 +7,13 @@ export const initiateApp = (app, express) => {
   app.use(express.json());
   connectDB();
   app.get("/", (req, res) => res.send("Hello World"));
- 
-  // app.use(
-  //   cors({
-  //     origin: "https://solarease.vercel.app/",
-  //     credentials: true,
-  //   })
-  // );
-  app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+  app.use(
+    cors({
+      origin: "https://solarease.vercel.app",
+      credentials: true,
+    })
+  );
+  // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
   app.use("/api/auth", allRoutes.authRouter);
   app.use("/api/blogs", allRoutes.blogRouter);
   app.use("/api/brands", allRoutes.brandRouter);
