9c336b63ba9cb0431858e91669937f3d44c41223
Ahmed Mohsen <am6945@fayoum.edu.eg>
2024-12-08 14:17:09 +0200

    Refactor CORS preflight handling in server.js to allow all origins and remove commented-out code

--- Back-end/server.js
+++ Back-end/server.js
@@ -3,18 +3,19 @@ import { config } from "dotenv";
 import path from "path";
 import { initiateApp } from "./Src/Utils/initiateApp.js";
 const app = express();
-app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://solarease.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, x-requested-with"
-  );
-  res.sendStatus(200); // Respond to preflight request
-});
+// app.options("*", (req, res) => {
+//   res.header("Access-Control-Allow-Origin", "https://solarease.vercel.app");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, x-requested-with"
+//   );
+//   res.sendStatus(200); // Respond to preflight request
+// });
+console.log("Hello World");
 app.use(
   cors({
-    origin: "https://solarease.vercel.app",
+    origin: "*",
     credentials: true,
   })
 );
