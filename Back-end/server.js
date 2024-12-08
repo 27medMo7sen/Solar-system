7054718092d1d1c17455e12f1032c6216dd7d030
Ahmed Mohsen <am6945@fayoum.edu.eg>
2024-12-08 14:18:57 +0200

    Add CORS middleware to server.js for handling cross-origin requests

--- Back-end/server.js
+++ Back-end/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import path from "path";
 import { initiateApp } from "./Src/Utils/initiateApp.js";
+import cors from "cors";
 const app = express();
 // app.options("*", (req, res) => {
 //   res.header("Access-Control-Allow-Origin", "https://solarease.vercel.app");
