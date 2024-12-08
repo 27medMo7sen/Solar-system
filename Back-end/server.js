e5f4223525896804948e996dfe7353aea2fa77a3
Ahmed Mohsen <am6945@fayoum.edu.eg>
2024-12-08 14:13:03 +0200

    Move CORS preflight handling from index.js to server.js and restore middleware configuration

--- Back-end/server.js
+++ Back-end/server.js
@@ -3,5 +3,20 @@ import { config } from "dotenv";
 import path from "path";
 import { initiateApp } from "./Src/Utils/initiateApp.js";
 const app = express();
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "https://solarease.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, x-requested-with"
+  );
+  res.sendStatus(200); // Respond to preflight request
+});
+app.use(
+  cors({
+    origin: "https://solarease.vercel.app",
+    credentials: true,
+  })
+);
 config({ path: path.resolve(".env") });
 initiateApp(app, express);
