fa772fb995c710224bbc927b2bcdcd20270b8822
Ahmed Mohsen <am6945@fayoum.edu.eg>
2024-12-08 13:45:48 +0200

    Add CORS preflight handling in index.js for all origins

--- Back-end/index.js
+++ Back-end/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import { config } from "dotenv";
 import { initiateApp } from "./Src/Utils/initiateApp.js";
 const app = express();
+app.options("*", cors());
+
 app.use(
     cors({
       origin: "https://solarease.vercel.app",
