38ff081f9c967e409c000f87576fb5ce16d49f54
Ahmed Mohsen <am6945@fayoum.edu.eg>
2024-12-10 01:41:55 +0200

    Refactor confirmation page to use axios for API requests and streamline response handling

--- Front-end/src/pages/Confirmation.jsx
+++ Front-end/src/pages/Confirmation.jsx
@@ -1,30 +1,25 @@
 import { useLoaderData } from "react-router-dom";
 import { Celebration } from "../components/Forms/Celebration";
-
+import axios from "axios";
 export const Confirmation = () => {
-    const data= useLoaderData();
-    console.log(data);
-    if(data.status === 200)
-        return <Celebration message={"Congratulations! You have successfully created an account."}/>
-     
-    return <Celebration message={"your account is already confirmed, login now."}/>
+  const data = useLoaderData();
+  console.log(data);
+  if (data.status === 200)
+    return (
+      <Celebration
+        message={"Congratulations! You have successfully created an account."}
+      />
+    );
 
-       
-    
+  return (
+    <Celebration message={"your account is already confirmed, login now."} />
+  );
 };
 export const loader = async ({ request }) => {
-    const token = new URL(request.url).pathname.split("/").pop();;
-    console.log(token);
-    const res = await fetch( 
-       `http://localhost:3000/api/auth/confirm/${token}`,
-         {
-              method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }
-        );
-        console.log(res);
-    return {message: res.message , status:res.status}; 
-}
+  const token = new URL(request.url).pathname.split("/").pop();
+  console.log(token);
+  const res = await axios.get(`/auth/confirm/${token}`);
 
+  console.log(res);
+  return { message: res.message, status: res.status };
+};
